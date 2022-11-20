a580570971f064304712f91328bd4a119979d8cb
Ilya Romanov <112113701+IlyaChef@users.noreply.github.com>
2022-11-20 19:37:07 +0300

    Add files via upload

---
+++ lists_homework.py
@@ -0,0 +1,11 @@
+numbers = [3, 5, 7, 9, 10.5]
+print(numbers)
+numbers.append('Python')
+print(len(numbers))
+
+print(numbers[0])
+print(numbers[-1])
+print(numbers[1:5])
+print(numbers[-1])
+del numbers[-1]
+print(numbers)
